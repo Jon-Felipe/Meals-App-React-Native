65b015e1d2ccedbe5f74e44e3b361db21807b450
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-31 09:50:41 +0200

    implementing drawer navigation and new favorites screen

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 // screens
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverview from './screens/MealsOverview';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name='Categories' component={CategoriesScreen} />
+      <Drawer.Screen name='Favorites' component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 function App() {
   return (
@@ -17,14 +29,16 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name='MealsCategories'
-            component={CategoriesScreen}
-            options={{
-              title: 'All Categories',
-            }}
+            name='Drawer'
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           />
           <Stack.Screen name='MealsOverview' component={MealsOverview} />
-          <Stack.Screen name='MealDetail' component={MealDetailScreen} />
+          <Stack.Screen
+            name='MealDetail'
+            component={MealDetailScreen}
+            options={{ title: 'About the meal' }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
