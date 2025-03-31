55ea5a64986a28d66f582496c4285e9e017a2c5d
Jon-Felipe <rodriguesjf7@gmail.com>
2025-03-31 11:05:39 +0200

    favorite icon click use layout effect dependency array update

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -40,7 +40,7 @@ export default function MealDetailScreen({ route, navigation }) {
         );
       },
     });
-  }, [navigation, changeFavoriteStatusHandler]);
+  }, [ids]);
 
   return (
     <ScrollView style={styles.rootContainer}>
